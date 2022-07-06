9704ce7322a926d13e4c9d76cc0ae83bac608c4a
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2022-07-06 16:36:10 +0700

    a

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.SECRET,
+});
